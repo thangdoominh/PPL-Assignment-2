ba53ae1d74f19e89be47585a474996a40b85e738
Do Minh Thang <dominhthang1999@gmail.com>
2019-10-14 17:55:25 +0700

    Vardeclaration
    Generation + 10 test VarDecl

--- initial/src/main/mc/astgen/ASTGeneration.py
+++ initial/src/main/mc/astgen/ASTGeneration.py
@@ -9,19 +9,36 @@ from AST import *
 
 class ASTGeneration(MCVisitor):
     def visitProgram(self,ctx:MCParser.ProgramContext):
-        return Program(self.visit(x) for x in ctx.declaration())
+        list_declaration = [self.visit(ctx.getChild(i)) for i in range(ctx.getChildCount()-1)]
+        declarations = []
+        for x in list_declaration:
+            if isinstance(x, list):
+                declarations += x
+            else:
+                declarations += [x]
+        return Program(declarations)
 
+    # ------------  Vardeclaration  ------------------
     def visitDeclaration(self, ctx:MCParser.DeclarationContext):
         return self.visit(ctx.getChild(0))
 
     def visitVardeclaration(self, ctx:MCParser.VardeclarationContext):
-        return VarDecl(self.visit(ctx.idlist()),self.visit(ctx.singletype()))
+        listVardec = []
+        typeVariable =  self.visit(ctx.singletype())
+        list_id = self.visit(ctx.idlist())
+        for x in list_id:
+            if not isinstance(x, list):
+                listVardec += [VarDecl(x, typeVariable)]
+
+            else:
+                listVardec += [VarDecl(x[0], ArrayType(x[1], typeVariable))]
+        return listVardec
 
     def visitSingletype(self, ctx:MCParser.SingletypeContext):
         if ctx.INTTYPE():
             return IntType()
         elif ctx.FLOATTYPE():
-            return VoidType()
+            return FloatType()
         elif ctx.BOOLTYPE():
             return BoolType()
         else:
@@ -33,21 +50,20 @@ class ASTGeneration(MCVisitor):
     def visitIdtail(self, ctx:MCParser.IdtailContext):
         return self.visit(ctx.getChild(0))
 
-    # def visitMctype(self,ctx:MCParser.MctypeContext):
-    #     if ctx.INTTYPE():
-    #         return IntType
-    #     else:
-    #         return VoidType
-    #
-    # def visitBody(self,ctx:MCParser.BodyContext):
-    #     return self.visit(ctx.funcall())
-    #
-  	#
-    # def visitFuncall(self,ctx:MCParser.FuncallContext):
-    #     return CallExpr(Id(ctx.ID().getText()),[self.visit(ctx.exp())] if ctx.exp() else [])
-    #
-    # def visitExp(self,ctx:MCParser.ExpContext):
-    #     if (ctx.funcall()):
-    #         return self.visit(ctx.funcall())
-    #     else:
-    #         return IntLiteral(int(ctx.INTLIT().getText()))
+    # # idarray: ID LSB INTLIT RSB;
+    def visitIdarray(self, ctx:MCParser.IdarrayContext):
+        return [ctx.ID().getText(), ctx.INTLIT().getText()]
+
+    def visitIdsingle(self, ctx:MCParser.IdsingleContext):
+        return ctx.ID().getText()
+
+    # ------------  Funcdeclaration  ------------------
+    def visitFuncdeclaration(self,ctx:MCParser.FuncdeclarationContext):
+        if ctx.VOIDTYPE():
+            typeFunc = VoidType()
+        else:
+            typeFunc = self.visit(ctx.getChild(0))
+
+    # ---- Array Pointer Type  -----
+    def visitArraypointertype(self,ctx:MCParser.ArraypointertypeContext):
+        if ctx.
--- initial/src/test/ASTGenSuite.py
+++ initial/src/test/ASTGenSuite.py
@@ -6,11 +6,106 @@ class ASTGenSuite(unittest.TestCase):
     def test_simple_program(self):
         """Simple program: int main() {} """
         input = """int a;"""
-        expect = str(Program([FuncDecl(Id("main"),[],IntType,Block([]))]))
+        expect = str(Program([VarDecl("a",IntType())]))
         self.assertTrue(TestAST.checkASTGen(input,expect,300))
 
     def test_declaration_301(self):
-        """Simple program: int main() {} """
         input = """string a, b;"""
-        expect = str(Program([FuncDecl(Id("main"),[],IntType,Block([]))]))
+        expect = str(Program([VarDecl("a",StringType()),VarDecl("b",StringType())]))
         self.assertTrue(TestAST.checkASTGen(input,expect,301))
+
+    def test_VarDec_302(self):
+        input = """int x,a[3];"""
+        expect = str(Program([VarDecl("x",IntType()),VarDecl("a",ArrayType(3,IntType()))]))
+        self.assertTrue(TestAST.checkASTGen(input,expect,302))
+
+    def test_VarDec_Float_303(self):
+        input = """float a,b,c;"""
+        expect = str(Program([VarDecl("a",FloatType()),
+                              VarDecl("b",FloatType()),
+                              VarDecl("c",FloatType())]))
+        self.assertTrue(TestAST.checkASTGen(input,expect,303))
+
+    def test_VarDec_304(self):
+        input = """float x, y[10], z"""
+        expect = str(Program([VarDecl("x",FloatType()),
+                              VarDecl("y",ArrayType(10,FloatType())),
+                              VarDecl("z",FloatType())]))
+        self.assertTrue(TestAST.checkASTGen(input,expect,304))
+
+    def test_VarDec_305(self):
+        input = """int abc;
+                   string xyz; 
+                   float qwe;
+                """
+        expect = str(Program([VarDecl("abc",IntType()),
+                              VarDecl("xyz",StringType()),
+                              VarDecl("qwe",FloatType())]))
+        self.assertTrue(TestAST.checkASTGen(input,expect,305))
+
+    def test_vardec_306(self):
+        input = """
+                int a;
+                float b;
+                string c[1];
+                boolean d;
+                """
+        expect = str(Program([VarDecl("a",IntType()),
+                              VarDecl("b",FloatType()),
+                              VarDecl("c",ArrayType(1,StringType())),
+                              VarDecl("d",BoolType())]))
+        self.assertTrue(TestAST.checkASTGen(input,expect,306))
+
+    def test_VarDec_307(self):
+        input = """
+                boolean _a_[3], b;
+                string thang[10], thuan[12], Phong[9];
+                """
+        expect = str(Program([VarDecl("_a_",ArrayType(3,BoolType())),
+                              VarDecl("b",BoolType()),
+                              VarDecl("thang",ArrayType(10,StringType())),
+                              VarDecl("thuan",ArrayType(12,StringType())),
+                              VarDecl("Phong",ArrayType(9,StringType()))]))
+        self.assertTrue(TestAST.checkASTGen(input,expect,307))
+
+    def test__(self):
+        input = """
+
+                """
+        expect = str(Program([]))
+        self.assertTrue(TestAST.checkASTGen(input, expect, ))
+
+    def test__(self):
+        input = """
+
+                """
+        expect = str(Program([]))
+        self.assertTrue(TestAST.checkASTGen(input, expect, ))
+
+    def test__(self):
+        input = """
+
+                """
+        expect = str(Program([]))
+        self.assertTrue(TestAST.checkASTGen(input, expect, ))
+
+    def test__(self):
+        input = """
+
+                """
+        expect = str(Program([]))
+        self.assertTrue(TestAST.checkASTGen(input, expect, ))
+
+    def test__(self):
+        input = """
+
+                """
+        expect = str(Program([]))
+        self.assertTrue(TestAST.checkASTGen(input, expect, ))
+
+    def test__(self):
+        input = """
+
+                """
+        expect = str(Program([]))
+        self.assertTrue(TestAST.checkASTGen(input, expect, ))
